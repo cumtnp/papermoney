2bdb3876134a4231110ee6b0c81f01c9878b0ea0
liang ding <cumtnp@gmail.com>
2023-11-15 16:22:36 +0800

    can run

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = 3034;
 
 // 提供public目录下的静态文件
 app.use(express.static('public'));
+app.use('/data-private', express.static('data-private'));
 
 app.listen(port, () => {
   console.log(`服务器正在 http://localhost:${port} 上监听`);
